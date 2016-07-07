f4837310d8d0536be2abaf8b9ee3e39396537361
ankikkos <ioannou_andreas@hotmail.gr>
2016-07-07 15:25:29 +0300

    Content Providers Course Completed.

--- app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
+++ app/src/main/java/android/example/com/dictionaryproviderexample/MainActivity.java
@@ -21,7 +21,8 @@ import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.provider.UserDictionary.Words;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 /**
  * This is the central activity for the Provider Dictionary Example App. The purpose of this app is
@@ -29,18 +30,45 @@ import android.widget.TextView;
  */
 public class MainActivity extends ActionBarActivity {
 
+    private static final String[] COLUMNS_TO_BE_BOUND = new String[]{Words.WORD, Words.FREQUENCY,};
+
+    private static final int[] LAYOUT_ITEMS_TO_FILL = new int[] {android.R.id.text1, android.R.id.text2,};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Get the TextView which will be populated with the Dictionary ContentProvider data.
-        TextView dictTextView = (TextView) findViewById(R.id.dictionary_text_view);
+        ListView dictListView = (ListView) findViewById(R.id.dictListView);
 
         // Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver = getContentResolver();
 
         // Get a Cursor containing all of the rows in the Words table
         Cursor cursor = resolver.query(UserDictionary.Words.CONTENT_URI, null, null, null, null);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, cursor,COLUMNS_TO_BE_BOUND, LAYOUT_ITEMS_TO_FILL, 0);
+
+        dictListView.setAdapter(adapter);
+
+//        try{
+//            dictTextView.setText("The UserDictionary contains " + cursor.getCount() + " words\n");
+//            dictTextView.append("COLUMNS: " + Words._ID + " - " + Words.FREQUENCY + " - " + Words.WORD);
+//            int idColumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+//            int frequencyColumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+//            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+//            while(cursor.moveToNext()){
+//
+//                int id = cursor.getInt(idColumn);
+//                int freq = cursor.getInt(frequencyColumn);
+//                String word = cursor.getString(wordColumn);
+//
+//                dictTextView.append("\n" + id + " - " + freq + " - " + word);
+//            }
+//        }finally {
+//            cursor.close();
+//        }
+
     }
 }
